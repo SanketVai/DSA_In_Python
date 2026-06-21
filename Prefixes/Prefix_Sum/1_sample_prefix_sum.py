3793c056aeaaf5f217b880047189f3b9c0fec2fd
Sanket  Vaidya <sanket.vaidya2023@gmail.com>
2026-06-21 13:10:07 +0530

    Basic Prefix and Postfix practise problems

--- Prefixes/Prefix_Sum/1_sample_prefix_sum.py
+++ Prefixes/Prefix_Sum/1_sample_prefix_sum.py
@@ -1,3 +1,4 @@
+# Build the prefix sum array manually.
 # Input
 arr = [3, 1, 5, 2]
 
@@ -14,3 +15,16 @@ for i in range(1,len(arr)):
 
 print(f"Original Array is {arr}")
 print(f"psum is {psum}")
+
+# Using your prefix array, calculate: Sum(1,2)
+
+sum = psum[2] - psum[1-1]
+
+print(f"Sum of (1,2) is : {sum}")
+
+# Q1 Sum(2,3)
+print(f"Sum of (2,3) is {psum[3] - psum[1]}")
+
+
+# Q2 Sum(0,2)
+print(f"Sum of (0,2) {psum[2] - psum[0]}")
